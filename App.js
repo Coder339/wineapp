b40e30f4e299a5802bcd8a4399bedc7148d11631
vikas <singh339vikas@gmail.com>
2021-03-08 20:28:36 +0530

    bugs resolved

--- App.js
+++ App.js
@@ -7,15 +7,10 @@ import {
 import { Provider } from 'react-redux';
 import store from './app/redux/store';
 import RootStackScreen from './app/config/router';
-import Landing from './app/screens/landing';
-import Splash from './app/screens/splash';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './app/screens/login';
 import { colors } from './app/assets/globalstyleconstants';
 // import { AuthContext } from './app/config/authcontext';
-import PushNotification from "react-native-push-notification";
 import HandleNotification from './app/screens/handlenotification';
-import OnboardingScreen from './app/screens/onboarding';
 import Welcome from './app/screens/welcome';
 
 const App = () => {
